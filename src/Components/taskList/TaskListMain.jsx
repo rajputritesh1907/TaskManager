a2ca22dd294300bc6ed8370a2cec17787814a61c
Ritesh Singh <rajputritesh1907@gmail.com>
2025-11-21 12:38:54 +0530

    rename TaskList to TaskListMain to fix casing issue

---
+++ src/Components/taskList/TaskListMain.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
+
+const TaskListMain = ({ data }) => {
+    return (
+        <div id='tasklist' className='h-auto md:h-[50%] flex flex-col md:flex-row items-center justify-start gap-5 flex-nowrap w-full py-1 mt-10 task-scroll overflow-x-auto'>
+            {data.tasks.map((elem, idx) => {
+                if (elem.active) {
+                    return <AcceptTask key={idx} data={elem} />
+                }
+                if (elem.newTask) {
+                    return <NewTask key={idx} data={elem} />
+                }
+                if (elem.completed) {
+                    return <CompleteTask key={idx} data={elem} />
+                }
+                if (elem.failed) {
+                    return <FailedTask key={idx} data={elem} />
+                }
+
+            })}
+        </div>
+    )
+}
+
+export default TaskListMain
